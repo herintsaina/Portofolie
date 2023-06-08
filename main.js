dce7e266a9de6648b663e29177bf06e03dc9d51f
Herintsaina <herintsainarak024@gmail.com>
2023-06-08 20:52:43 +0300

    Ajouter de tout le fiechier

---
+++ main.js
@@ -0,0 +1,12 @@
+
+window.addEventListener('scroll', menuScroll);
+
+function menuScroll() {
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition > 100) {
+        document.getElementById('menu').classList.add('menu-scroll');
+    }else {
+        document.getElementById('menu').classList.remove('menu-scroll');
+    }
+}
